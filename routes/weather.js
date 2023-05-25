7d0a67b795fb58e76dbb1332a2fd1377c813a91a
GreenUpBuddy <105259071+GreenUpBuddy@users.noreply.github.com>
2023-05-25 11:08:37 -0700

    added lat and lon as well as weather documentation

--- routes/weather.js
+++ routes/weather.js
@@ -6,6 +6,8 @@ const router = express.Router()
 const msg_functions = require('../utilities/exports').messaging
 const validation = require('../utilities').validation
 let isStringProvided = validation.isStringProvided
+
+
 //Handler for processing async external get requests.
 //Source: https://gist.github.com/msmfsd/fca50ab095b795eb39739e8c4357a808
 async function fetchAsync (u) {
@@ -13,30 +15,105 @@ async function fetchAsync (u) {
     let data = await response.json();
     return data;
 }
-router.get("/", (request, response, next) => {
-    if (isStringProvided(request.query.zipcode) && isStringProvided(request.query.forecast)) {
+
+/**
+ * @apiDefine JSONError
+ * @apiError (400: JSON Error) {String} message "malformed JSON in parameters"
+ */ 
+
+/**
+ * @api {get} /weather Request to add a chat
+ * @apiName GetWeather
+ * @apiGroup Weather
+ * 
+ * @apiHeader {String} authorization Valid JSON Web Token JWT
+ * @apiParam {String} must provide city, latitude and longitude, or zipcode
+ * @apiParam {String} provide current, hourly, or daily as forecast argument
+ * 
+ * 
+ * @apiError (400: Missing Parameters) {String} message "Missing required information"
+ * 
+ * @apiError (400: Bad Forecast Argument) {String} Invalid argument for forecast
+ * 
+ * 
+ * @apiUse JSONError
+ */ 
+router.post("/", (request, response, next) => {
+    if ((isStringProvided(request.body.zipcode) || isStringProvided(request.body.city) || isStringProvided(request.body.latlong))
+     && isStringProvided(request.body.forecast)) {
         next();
     } else {
         response.status(400).send({
           message: "Missing required information",
         });
     }
-}, (request, response) => {
-    let forecast = request.query.forecast
+}, (request, response,next) => {
+    let city = request.body.city
+    let zipcode = request.body.zipcode
+    let latlong = request.body.latlong
+    let forecast = request.body.forecast
+    // console.log("city: " + city + "\n"
+    //  + "zipcode: " + zipcode + "\n"
+    //  + "latlong: " + latlong + "\n"
+    //  + "forecast: " + forecast)
     let url
-
-    if (forecast == "current"){
-        url = "https://api.weatherbit.io/v2.0/current?city=Tacoma,WA&units=I&key=3533b5d42baa4fa5b8dbdebd88798450"
-    }else if (forecast == "daily"){
-        url = "https://api.weatherbit.io/v2.0/forecast/daily?city=Tacoma,WA&units=I&key=3533b5d42baa4fa5b8dbdebd88798450" 
-    }else if (forecast == "hourly"){
-        url = "https://api.weatherbit.io/v2.0/forecast/hourly?city=Tacoma,WA&units=I&key=3533b5d42baa4fa5b8dbdebd88798450&hours=24"
+    
+    if (forecast == "current" && city != "NA"){
+        url = "https://api.weatherbit.io/v2.0/current?city=" + city + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450"
+        request.url = url
+        next();
+    }else if (forecast == "daily" && city != "NA"){
+        url = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + city + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450" 
+        request.url = url
+        next();
+    }else if (forecast == "hourly" && city != "NA"){
+        url = "https://api.weatherbit.io/v2.0/forecast/hourly?city=" + city + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450&hours=24"
+        request.url = url
+        next();
+    }else if (forecast == "current" && zipcode != "NA"){
+        url = "https://api.weatherbit.io/v2.0/current?postal_code=" + zipcode + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450"
+        request.url = url
+        next();
+    }else if (forecast == "daily" && zipcode != "NA"){
+        url = "https://api.weatherbit.io/v2.0/forecast/daily?postal_code=" + zipcode + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450" 
+        request.url = url
+        next();
+    }else if (forecast == "hourly" && zipcode != "NA"){
+        url = "https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=" + zipcode + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450&hours=24"
+        request.url = url
+        next();
+    }else if (forecast == "current" && latlong != "NA"){
+        let temp = latlong.split(",")
+        //console.log(temp)
+        let lat = temp[0]
+        let lon = temp[1]
+        url = "https://api.weatherbit.io/v2.0/current?lat=" + lat + "&lon=" + lon +"&units=I&key=3533b5d42baa4fa5b8dbdebd88798450"
+        request.url = url
+        next();
+    }else if (forecast == "daily" && latlong != "NA"){
+        let temp = latlong.split(",")
+        //console.log(temp)
+        let lat = temp[0]
+        let lon = temp[1]
+        url = "https://api.weatherbit.io/v2.0/forecast/daily?lat=" + lat + "&lon=" + lon + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450"
+        request.url = url
+        next();
+    }else if (forecast == "hourly" && latlong != "NA"){
+        let temp = latlong.split(",")
+        //console.log(temp)
+        let lat = temp[0]
+        let lon = temp[1]
+        url = "https://api.weatherbit.io/v2.0/forecast/hourly?lat=" + lat+ "&lon=" + lon + "&units=I&key=3533b5d42baa4fa5b8dbdebd88798450&hours=24"
+        request.url = url
+        next();
     }else{
         response.status(400).send({
             message: "Invalid argument for forecast."
         })
     }
-    fetchAsync(url)
+},(request, response) => {
+    //console.log(request.url)
+    fetchAsync(request.url)
     .then(data => response.status(200).send(data))
     .catch(reason => response.status(400).send(reason.message));
 });
